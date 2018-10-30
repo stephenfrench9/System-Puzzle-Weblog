6a19c515a4e68c53e11d8e0fb1cd9c015731f375
Stephen French <stephenfrench9@gmail.com>
2018-10-29 22:08:17 -0700

    remove sanity checks from website, only show final answer

--- app.py
+++ app.py
@@ -26,12 +26,6 @@ def index():
     cur.execute(sql_success)
     success = cur.fetchone()[0]
 
-    # Get the number of failed attempts
-    sql_fail = """SELECT COUNT(*) FROM weblogs 
-    WHERE status NOT LIKE \'2__\';"""
-    cur.execute(sql_fail)
-    fail = cur.fetchone()[0]
-
     # find all the local requests
     sql_location = """SELECT COUNT(*) FROM weblogs 
     WHERE source LIKE \'0\';"""
@@ -44,23 +38,11 @@ def index():
     cur.execute(sql_location)
     remote = cur.fetchone()[0]
 
-    # find all the exotic requests
-    sql_location = """SELECT COUNT(*) FROM weblogs 
-    WHERE source LIKE \'2\';"""
-    cur.execute(sql_location)
-    exotic = cur.fetchone()[0]
-
     # Get the number of successful local attempts
     sql_fail = """SELECT COUNT(*) FROM weblogs 
     WHERE status LIKE \'2__\' AND source LIKE \'0\';"""
     cur.execute(sql_fail)
     success_local = cur.fetchone()[0]
-
-    # Get the number of failed local attempts
-    sql_fail = """SELECT COUNT(*) FROM weblogs 
-    WHERE status NOT LIKE \'2__\' AND source LIKE \'0\';"""
-    cur.execute(sql_fail)
-    fail_local = cur.fetchone()[0]
 
     # Get the number of successful remote attempts
     sql_fail = """SELECT COUNT(*) FROM weblogs 
@@ -68,48 +50,18 @@ def index():
     cur.execute(sql_fail)
     success_remote = cur.fetchone()[0]
 
-    # Get the number of failed remote attempts
-    sql_fail = """SELECT COUNT(*) FROM weblogs 
-    WHERE status NOT LIKE \'2__\' AND source LIKE \'1\';"""
-    cur.execute(sql_fail)
-    fail_remote = cur.fetchone()[0]
-
     rate = "No entries yet!"
     if all != 0:
-        total = success + fail
-        total2 = local + remote + exotic
         rate = success / all
+        local_rate = success_local / local
+        remote_rate = success_remote / remote
 
-        local_sum = success_local + fail_local
-        remote_sum = success_remote + fail_remote
-        local_rate = success_local/local
-        remote_rate = success_remote/remote
-
-        all = str(all)
-        success = str(success)
-        fail = str(fail)
         rate = str(rate)
-        total = str(total)
-        local = str(local)
-        remote = str(remote)
-        exotic = str(exotic)
-        total2 = str(total2)
-        success_local = str(success_local)
-        fail_local = str(fail_local)
-        success_remote = str(success_remote)
-        fail_remote = str(fail_remote)
-        local_sum = str(local_sum)
-        remote_sum = str(remote_sum)
         local_rate = str(local_rate)
         remote_rate = str(remote_rate)
 
-    return render_template('index.html', rate=rate, all=all, success=success,
-                           failure=fail, total=total, local=local,
-                           remote=remote, exotic=exotic, total2=total2,
-                           success_local=success_local, fail_local=fail_local,
-                           success_remote=success_remote,
-                           fail_remote=fail_remote, local_sum=local_sum,
-                           remote_sum=remote_sum, local_rate=local_rate,
+    return render_template('index.html', rate=rate,
+                           local_rate=local_rate,
                            remote_rate=remote_rate)
 
 if __name__ == '__main__':
